f0b34dbcc8437ea854199c6f9b033743f8550f29
Сурганов Денис <surganov.denis@gmail.com>
2025-03-11 15:43:24 +0500

    2 пункт работает

--- index.js
+++ index.js
@@ -21,22 +21,23 @@ function animaster() {
 }
 
 function addListeners() {
+    const animasterObj = animaster();
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animasterObj.fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animasterObj.move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animasterObj.scale(block, 1000, 1.25);
         });
 }
 
